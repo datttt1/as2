81aed8f10ccfffb88a86c6c83b7a34603a655585
datttt1 <tranquangdat3275@gmail.com>
2025-11-27 23:01:17 +0700

    backend test2

---
+++ src/backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.userDTO.LoginResponse;
+import com.example.demo.dto.userDTO.UserLoginRequest;
+import com.example.demo.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public LoginResponse login (@RequestBody UserLoginRequest req) {
+        return authService.authenticate(req);
+    }
+}
---
+++ src/backend/src/main/java/com/example/demo/dto/userDTO/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.example.demo.dto.userDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+    private boolean success;
+    private String message;
+    private String token;
+}
