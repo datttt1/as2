b55fc0af7f62fde3ce3b31f354e03540835ad1c0
datttt1 <tranquangdat3275@gmail.com>
2025-11-27 21:30:27 +0700

    auth

---
+++ src/backend/src/main/java/com/example/demo/service/AuthService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.userDTO.LoginResponse;
+import com.example.demo.dto.userDTO.UserLoginRequest;
+
+public class AuthService {
+    public LoginResponse authenticate (UserLoginRequest userLoginRequest) {
+        if(!isValidRequest(userLoginRequest)) {
+            return new LoginResponse(false, "Validation failed", null);
+        }
+
+        if(!"testuser".equals(userLoginRequest.getUsername())) {
+            return new LoginResponse(false, "Username not found", null);
+        }
+
+        if(!"testuser123".equals(userLoginRequest.getPassword())) {
+            return new LoginResponse(false, "Wrong Password", null);
+        }
+
+        return new LoginResponse(true, "Login Success", "FAKE_TOKEN_123");
+    }
+
+
+public boolean isValidRequest (UserLoginRequest req) {
+
+    return req != null && 
+            req.getUsername() !=null &&
+            req.getPassword() != null &&
+            req.getUsername().length() >= 3 &&
+            req.getPassword().length() >= 6;
+}
+}
