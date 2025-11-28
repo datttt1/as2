3339f78cdb7331ed52513d05b95e58f5c0ec45a7
xuannhat999 <xuannhat123456789@gmail.com>
2025-11-28 22:39:47 +0700

    update login ci/cd

--- src/frontend/cypress.config.js
+++ src/frontend/cypress.config.js
@@ -1,10 +1,12 @@
-import  { defineConfig }  from "cypress";
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-      baseUrl: "http://localhost:5173",
-      viewportWidth: 1280,
-      viewportHeight: 720,
-      video: false,
+    baseUrl: "http://localhost:5173",
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    supportFile: false, // tắt support file
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
   },
 });
