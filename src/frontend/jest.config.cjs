4bebd2793ffdffa6de9546a7576504302ff6e3c6
xuannhat999 <xuannhat123456789@gmail.com>
2025-11-30 18:00:49 +0700

    fix generate report

--- src/frontend/jest.config.cjs
+++ src/frontend/jest.config.cjs
@@ -1,18 +1,15 @@
-module.exports= {
+module.exports = {
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.jsx?$": "babel-jest"
   },
   setupFiles: ["<rootDir>/setupJestEnv.js"],
   setupFilesAfterEnv: ["<rootDir>/setupTestsAfterEnv.js"],
-   reporters: [
+  reporters: [
     "default",
-    [
-      "jest-junit",
-      {
-        outputDirectory: "test-results",
-        outputName: "junit.xml"
-      }
-    ]
+    ["jest-html-reporter", {
+      "outputPath": "./reports/test-report.html",
+      "pageTitle": "Test Report"
+    }]
   ]
 };
